0974e247e9f3499d77410e397e60c881d3274487
royanaditya <royanaditya8@gmail.com>
2025-06-03 23:07:03 +0700

    fix models of product

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -4,21 +4,33 @@ class Product {
   final double price;
   final String image;
   int quantity;
+  final String? brand;
+  final String? category;
+  final double? rating;
+
 
   Product({
-    required this.id,
-    required this.name,
-    required this.price,
-    required this.image,
-    this.quantity = 1,
-  });
+  required this.id,
+  required this.name,
+  required this.price,
+  required this.image,
+  this.quantity = 1,
+  this.brand,
+  this.category,
+  this.rating,
+});
 
-  factory Product.fromJson(Map<String, dynamic> json) {
-    return Product(
-      id: json['id'],
-      name: json['name'],
-      price: double.parse(json['price'].toString()),
-      image: json['image'],
-    );
-  }
+factory Product.fromJson(Map<String, dynamic> json) {
+  return Product(
+    id: json['id'],
+    name: json['name'],
+    price: double.parse(json['price'].toString()),
+    image: json['image'],
+    quantity: json['quantity'] ?? 1,
+    brand: json['brand'],
+    category: json['category'],
+    rating: json['rating'] != null ? double.tryParse(json['rating'].toString()) : null,
+  );
+}
+
 }
